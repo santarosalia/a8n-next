34af4c437a3b6e0157b86726d485a5e018900e4d
gqob <dhkim@edentns.com>
2023-09-13 17:56:20 +0900

    a

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -3,8 +3,8 @@ import GoogleProvider from "next-auth/providers/google";
 const handler = NextAuth({
     providers : [
         GoogleProvider({
-            clientId : '639298869227-74ado9k12oc3sh0vc6d1s68kbu71sl35.apps.googleusercontent.com',
-            clientSecret : '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'
+            clientId : '',
+            clientSecret : ''
         })
     ]
 });
