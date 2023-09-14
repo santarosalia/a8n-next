51d9139a773f8423891ec12f492219dd3f69da36
gqob <dhkim@edentns.com>
2023-09-14 16:00:39 +0900

    signinloadingedit

--- src/components/HeaderSettings.tsx
+++ src/components/HeaderSettings.tsx
@@ -1,6 +1,5 @@
-"use client"
 
-import { Avatar, Box, Button, IconButton, Menu, MenuItem, Tooltip, Typography } from "@mui/material";
+import { Avatar, Box, Button, CircularProgress, IconButton, Menu, MenuItem, Tooltip, Typography } from "@mui/material";
 import { signIn, signOut, useSession } from "next-auth/react";
 import React from "react";
 
@@ -9,6 +8,7 @@ const settings = ['Profile', 'Account', 'Dashboard', 'Signout'];
 
 export default () => {
     const {data: session, status} = useSession();
+    
     const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(null);
     const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
         setAnchorElUser(event.currentTarget);
@@ -22,40 +22,39 @@ export default () => {
     const signOutSession = async () => {
         await signOut();
     }
+    
     if (status === 'loading') {
-        return (
-            <Box>Loading...</Box>
-        )
+        return <CircularProgress color="inherit" size={20}/>
     }
     if (session) {
         return (
-                    <Box sx={{ flexGrow: 0 }}>
-                        <Button onClick={handleOpenUserMenu} sx={{ p: 0 }} color="inherit">
-                            {session?.user?.name?.toUpperCase() ?? 'MENU'}
-                        </Button>
-                        <Menu
-                        sx={{ mt: '45px' }}
-                        id="menu-appbar"
-                        anchorEl={anchorElUser}
-                        anchorOrigin={{
-                            vertical: 'top',
-                            horizontal: 'right',
-                        }}
-                        keepMounted
-                        transformOrigin={{
-                            vertical: 'top',
-                            horizontal: 'right',
-                        }}
-                        open={Boolean(anchorElUser)}
-                        onClose={handleCloseUserMenu}
-                        >
-                        {settings.map((setting) => (
-                            <MenuItem key={setting} onClick={setting === 'Signout' ? signOutSession : handleCloseUserMenu}>
-                            <Typography textAlign="center">{setting}</Typography>
-                            </MenuItem>
-                        ))}
-                        </Menu>
-                    </Box>
+            <Box sx={{ flexGrow: 0 }}>
+                <Button onClick={handleOpenUserMenu} sx={{ p: 0 }} color="inherit">
+                    {session?.user?.name?.toUpperCase() ?? 'MENU'}
+                </Button>
+                <Menu
+                sx={{ mt: '45px' }}
+                id="menu-appbar"
+                anchorEl={anchorElUser}
+                anchorOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                }}
+                keepMounted
+                transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                }}
+                open={Boolean(anchorElUser)}
+                onClose={handleCloseUserMenu}
+                >
+                {settings.map((setting) => (
+                    <MenuItem key={setting} onClick={setting === 'Signout' ? signOutSession : handleCloseUserMenu}>
+                    <Typography textAlign="center">{setting}</Typography>
+                    </MenuItem>
+                ))}
+                </Menu>
+            </Box>
         )
     }
     return (
