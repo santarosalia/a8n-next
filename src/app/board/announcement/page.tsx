254bfc70707a68bb081a448ce92b3e6eeac78442
dope <dhkim@edentns.com>
2023-09-21 23:08:06 +0900

    board layout

--- src/app/board/announcement/page.tsx
+++ src/app/board/announcement/page.tsx
@@ -1,31 +1,89 @@
-import { Box, Divider, List, ListItem, ListItemButton, Typography } from "@mui/material"
+import { Box, Chip, Divider, List, ListItem, ListItemButton, Typography } from "@mui/material"
 
 export default () => {
+    enum Category {
+        ANNOUNCEMNET,
+        PROCESS,
+        
+    }
+    interface Comment {
+        content: string
+    }
+    interface Board {
+        index: number,
+        id: string
+        title: string,
+        profile: {
+            id: string,
+            name: string
+        },
+        createdAt: Date,
+        updatedAt: Date,
+        category: Category,
+        hashtag: string[],
+        readCount: number,
+        commentCount: number,
+        recommendation: number
+    }
+
+    const boards: Board[] = [];
+    for (let i = 0; i < 9; i++) {
+        boards.push(
+            {
+                index : i,
+                id : `id${i}`,
+                title : `test${i}`,
+                profile : {
+                    id : `profile id ${i}`,
+                    name : `name${i}`
+                },
+                createdAt : new Date(),
+                updatedAt : new Date(),
+                category : Category.PROCESS,
+                hashtag : [`#hash${i}`],
+                readCount : i,
+                commentCount : i,
+                recommendation : i
+            }
+        );
+    }
+    const BoardComponents = boards.map(board => {
+        return (
+            <>
+            <ListItem dense sx={{
+                height:'80px'
+            }}>
+                <Box>
+                    <Box>
+                        {board.profile.name} {`${board.createdAt.getFullYear()}-${board.createdAt.getMonth()+1}-${board.createdAt.getDate()} ${board.createdAt.getHours()}:${board.createdAt.getMinutes()}` }
+                    </Box>
+                    <Box>
+                        <Typography variant="h6">
+                            {board.title}
+                        </Typography>
+                    </Box>
+                    <Box>
+                        <Chip
+                        label={board.category}
+                        size="small">
+                        </Chip>
+                        {board.hashtag}
+                        {board.readCount}
+                        {board.commentCount}
+                        {board.recommendation}
+                    </Box>
+                </Box>
+            </ListItem>
+            <Divider></Divider>
+            </>
+        )
+    });
     return (
         <>
         
         <List>
             <Divider></Divider>
-            <ListItem dense sx={{
-                height:'80px'
-            }}>
-                <Box>
-                    <Box>profile , name , createdate, </Box>
-                    <Box>title</Box>
-                    <Box>category, hashtag , readcount, comment , reco</Box>
-                </Box>
-            </ListItem>
-            <Divider></Divider>
-            <ListItem dense sx={{
-                height:'80px'
-            }}>
-                <Box>
-                    <Box>d</Box>
-                    <Box>a</Box>
-                    <Box>c</Box>
-                </Box>
-            </ListItem>
-            <Divider></Divider>
+            {BoardComponents}
         </List>
         </>
     )
