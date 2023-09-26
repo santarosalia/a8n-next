674d779d8a262e186a6870e3c49c75c4dc881204
gqob <dhkim@edentns.com>
2023-09-26 17:48:21 +0900

    plans edit

--- src/app/plans/unlimited/page.tsx
+++ src/app/plans/unlimited/page.tsx
@@ -10,9 +10,9 @@ export default () => {
     const paperOnClick = (i: number) => {
         setCheckedPlan(i);
     }
-    const papers = unlimitedPlan.map(i => {
-        return (
-            <Paper
+
+    const paperss = (i: number) => {
+        <Paper
             component={'button'}
             variant="outlined"
             onClick={() => paperOnClick(i)}
@@ -34,9 +34,63 @@ export default () => {
                     xs : '70vw',
                     md : '100%'
                 },
-                height : '120px'
-
+                height : '120px',
             }}
+            ></Paper>
+    }
+    
+    const papers = unlimitedPlan.map(i => {
+        return (
+            <Paper
+            component={'button'}
+            variant="outlined"
+            onClick={() => paperOnClick(i)}
+            key={i}
+            {...(checkedPlan === i ? {
+                sx :{
+                    display : 'flex',
+                    flexDirection : 'column',
+                    flex : 1,
+                    margin : 1,
+                    marginX : {
+                        xs : 'auto',
+                        md : 1
+                    },
+                    padding : 1,
+                    ':hover' : {
+                        scale : '1.03'
+                    },
+                    width : {
+                        xs : '70vw',
+                        md : '100%'
+                    },
+                    height : '120px',
+                    borderColor : green[600],
+                    backgroundColor : green[50]
+                }
+            }
+            :
+            {
+                sx :{
+                    display : 'flex',
+                    flexDirection : 'column',
+                    flex : 1,
+                    margin : 1,
+                    marginX : {
+                        xs : 'auto',
+                        md : 1
+                    },
+                    padding : 1,
+                    ':hover' : {
+                        scale : '1.03'
+                    },
+                    width : {
+                        xs : '70vw',
+                        md : '100%'
+                    },
+                    height : '120px',
+                }
+            })}
             >
                 <Box display={'flex'}>
                     {checkedPlan === i ? 
@@ -47,10 +101,19 @@ export default () => {
                         period
                     </Typography>
                 </Box>
-                <Box>
-                    <Typography variant="h4" marginX={3}>
+                <Box display={'flex'}>
+                    {checkedPlan === i ?
+                    <Typography variant="h4" marginLeft={3} sx={{color : green[600]}}>
+                    price
+                    </Typography>
+                    :
+                    <Typography variant="h4" marginLeft={3}>
                         price
                     </Typography>
+                }
+                <Typography lineHeight={3.5}>
+                    /month
+                </Typography>
                 </Box>
                 <Box>
                     <Typography variant="subtitle2" marginX={3}>
@@ -63,13 +126,24 @@ export default () => {
     return (
         <Container maxWidth={'xl'}>
             <Box display={'flex'} justifyContent={'center'}>
+                
                 <Box display={'flex'} margin={3}
+                flexDirection={'column'}
                 sx={{
                     width : {
                         xs : '100%',
                         md : '80vw'
                     }
                 }}>
+                    <Box
+                    margin={3}
+                    textAlign={'center'}
+                    height={'100px'}
+                    >
+                        <Typography sx={{fontSize : { xs : 20, md : 30}}}>
+                            Get an unlimited LunaticMonster plan
+                        </Typography>
+                    </Box>
                    <Box
                    width={'100%'}
                    height={'360px'}
