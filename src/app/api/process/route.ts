b84abacb089bf57fec0ecb9972d997f50581a3ee
dope <dhkim@edentns.com>
2023-10-06 21:52:33 +0900

    process test

--- src/app/api/process/route.ts
+++ src/app/api/process/route.ts
@@ -1,7 +1,25 @@
 import { signJwtAccessToken, verifyJwt } from "@/app/lib/jwt";
 import prisma from "@/app/lib/prisma";
-import * as bcrypt from 'bcrypt';
 
+export const GET = async (req: Request) => {
+    const accessToken = req.headers.get('authorization');
+    if (!accessToken || !verifyJwt(accessToken)) {
+        return new Response(JSON.stringify({
+            error : 'No Authorization'
+        }),{
+            status : 401
+        });
+    }
+    const body = await req.json();
+    const userId = body.userId;
+
+    const process = await prisma.process.findMany({
+        where : {
+            userId : userId,
+        }
+    });
+    return new Response(JSON.stringify(process));
+}
 export const POST = async (req: Request) => {
     const accessToken = req.headers.get('authorization');
     if (!accessToken || !verifyJwt(accessToken)) {
@@ -13,19 +31,13 @@ export const POST = async (req: Request) => {
     }
     const body = await req.json();
     const id = body.id;
+    const userId = body.userId;
 
-    const processes = await prisma.process.findMany({
+    const process = await prisma.process.findUnique({
         where : {
-            userId : id,
-        },
-        include : {
-            user : {
-                select : {
-                    email: true,
-                    name: true
-                }
-            }
+            id : id,
+            userId : userId,
         }
     });
-    return new Response(JSON.stringify(processes));
+    return new Response(JSON.stringify(process));
 }
