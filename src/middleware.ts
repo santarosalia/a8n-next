e98500d6bbf7a1cd9e36a3c679a65eead3fad2b1
dope <dhkim@edentns.com>
2023-10-10 23:47:09 +0900

    console delete

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,6 @@
 import { NextRequest, NextResponse } from "next/server"
 
 export const middleware = (req: NextRequest) => {
-    console.log(req.cookies)
     const {pathname} = req.nextUrl;
     switch (pathname) {
         case '/board/write' : {
