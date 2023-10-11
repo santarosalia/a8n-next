e7180a9f6884205e97db8e3a9a7e5545922fc88f
gqob <dhkim@edentns.com>
2023-10-11 16:25:52 +0900

    middleware edit

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,13 @@
 import { NextRequest, NextResponse } from "next/server"
+import { getToken } from "next-auth/jwt";
 
-export const middleware = (req: NextRequest) => {
+const secret = process.env.SECRET_KEY;
+export const middleware = async (req: NextRequest) => {
     const {pathname} = req.nextUrl;
     switch (pathname) {
         case '/board/write' : {
+            const session = await getToken({req, secret, raw : true});
+            if (!session) return NextResponse.redirect(new URL('/', req.url));
         }
     }
 }
