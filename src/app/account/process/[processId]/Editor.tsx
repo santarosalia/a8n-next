2a3ba813c061bd4283a2d5fcdc865962589adc07
gqob <dhkim@edentns.com>
2023-11-05 23:52:05 +0900

    editor

--- src/app/account/process/[processId]/Editor.tsx
+++ src/app/account/process/[processId]/Editor.tsx
@@ -5,8 +5,10 @@ import { purple } from "@mui/material/colors";
 
 export default () => {
     const action = useAppSelector(getAction);
-    const params = action?.object.parameter;
-    console.log(params)
+    const params = action?.object.parameter ?? {};
+    Object.entries(params).map(([key, value])=> {
+
+    });
     return (
         <Box position={'fixed'} borderRadius={'5px 5px 0 0'} bottom={0} left={'50% - 400px'} height={200} width={'400px'} sx={{backgroundColor : purple[50]}}>
             <Typography>
