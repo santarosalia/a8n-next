e2f36df8e44c0c1b840563f1a81ea6fe63736a93
gqob <dhkim@edentns.com>
2024-08-02 23:35:11 +0900

    feat: thread start

--- src/app/Footer.tsx
+++ src/app/Footer.tsx
@@ -1,20 +1,32 @@
-import { Box, Container, Divider, Typography } from "@mui/material"
-import { pink } from "@mui/material/colors"
+import { Add, Favorite, Home, Person, Search } from "@mui/icons-material"
+import { AppBar, Box, Button, Container, Divider, Icon, Toolbar, Typography } from "@mui/material"
+import Image from "next/image"
+import Link from "next/link"
 
 export default () => {
     return (
-        <Box
-        textAlign={'center'}
-        height={'150px'}
-        display={'flex'}
-        alignItems={'center'}
-        justifyContent={'center'}
-        bgcolor={pink[50]}
-        marginTop={5}
-        >
-            <Typography>
-                Copyright ©  2023 Lunatic Monster All rights reserved.
-            </Typography>
-        </Box>
+        <AppBar position="fixed" variant="elevation" sx={{top: 'auto', bottom: '0', backgroundColor: 'black'}}>
+            <Container maxWidth="xl">
+                <Toolbar disableGutters>
+                    <Box display={'flex'} alignItems={'center'} width={'100%'} justifyContent={'space-between'}>
+                        <Button variant="text" color="inherit">
+                            <Home></Home>
+                        </Button>
+                        <Button variant="text" color="inherit">
+                            <Search></Search>
+                        </Button>
+                        <Button variant="text" color="inherit">
+                            <Add></Add>
+                        </Button>
+                        <Button variant="text" color="inherit">
+                            <Favorite></Favorite>
+                        </Button>
+                        <Button variant="text" color="inherit">
+                            <Person></Person>
+                        </Button>
+                    </Box>
+                </Toolbar>
+            </Container>
+        </AppBar>
     )
 }
