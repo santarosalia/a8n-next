a466809a8a97b734ea60eeee998563e2252b4c91
gqob <dhkim@edentns.com>
2024-08-04 13:27:43 +0900

    feat: list

--- src/app/Section2.tsx
+++ src/app/Section2.tsx
@@ -1,27 +1,37 @@
 import { Box, Container, Typography } from "@mui/material"
 import { grey, yellow } from "@mui/material/colors"
 
+const Item = () => {
+    return (
+        <Container>
+            <Typography variant="h4" display={'flex'} justifyContent={'space-between'}>
+                <span>😍</span>
+                <span>img</span>
+            </Typography>
+            <Typography variant="subtitle2" display={"flex"} justifyContent={'end'}>
+            id
+            </Typography>
+            <Typography variant="body1" height={100}>
+                Contents
+            </Typography>
+        </Container>
+    )
+}
+const l = [0,1,2,3,4,5,6,7,8,9];
+const items = l.map(i => {
+    return (<Item/>);
+})
 export default () => {
     return (
         <Container>
-            <Box color={grey[100]}
-            mt={1}
-            height={'200vh'} 
-            display={'flex'} 
+            <Box 
+                color={grey[100]}
+                mt={1}
+                height={'200vh'} 
+                display={'flex'}
+                flexDirection={'column'}
             >
-                <Container component={'div'}>
-                    <Typography variant="h4" display={'flex'} justifyContent={'space-between'}>
-                        <span>😍</span>
-                        <span>img</span>
-                    </Typography>
-                    <Typography variant="subtitle2" display={"flex"} justifyContent={'end'}>
-                    id
-                    </Typography>
-                    <Typography variant="body1">
-                        Contents
-                    </Typography>
-                </Container>
-                
+                {items}
             </Box>
         </Container>
     )
